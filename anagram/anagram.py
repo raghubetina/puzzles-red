5f94c80af97f79f6969c3b570404cc60fd0920bc
NobuyukiFuruta <nfuruta@uchicago.edu>
2020-09-14 20:11:16 +0000

    anagram2

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -6,16 +6,7 @@ def find_anagrams(word, candidates):
                 anagram_list.append(j)
     return (anagram_list)
 
-    #word_list = list(word)
-    #word_list.sort()
-    #candidates_list = list(candidates)
-    #candidates_list.sort()
-
-    #return (word_list == candidates_list)
-
 word = ["listen"]
 candidates = ["enlists", "google", "inlets", "banana"]
 
-#print(word)
-#print(candidates)
 print(find_anagrams(word, candidates))
