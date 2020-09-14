c874538f139779248bcceb6ab1f47e9628d58840
NobuyukiFuruta <nfuruta@uchicago.edu>
2020-09-14 19:01:14 +0000

    Anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,21 @@
 def find_anagrams(word, candidates):
-    pass
+    anagram_list = []
+    for i in word: 
+        for j in candidates: 
+            if i != j and (sorted(i)==sorted(j)):
+                anagram_list.append(j)
+    return (anagram_list)
+
+    #word_list = list(word)
+    #word_list.sort()
+    #candidates_list = list(candidates)
+    #candidates_list.sort()
+
+    #return (word_list == candidates_list)
+
+word = ["listen"]
+candidates = ["enlists", "google", "inlets", "banana"]
+
+#print(word)
+#print(candidates)
+print(find_anagrams(word, candidates))
