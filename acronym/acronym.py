5f101036dcac68f4891deca521b8996197fccc41
jmopsick <Jeremy.Mopsick@chicagobooth.edu>
2020-09-14 19:40:10 +0000

    finished-acronym-code

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,13 @@
 def abbreviate(words):
-    pass
+    i = 0
+    acronym = ""
+    while i <= len(words)-1: 
+        letter = words[i]
+        if i == 0: 
+            acronym = acronym + letter.upper()
+            i = i + 1
+        if words[i-1] == " ": 
+            acronym = acronym + letter.upper()
+            i = i + 1
+        i = i+1
+    print(acronym)
