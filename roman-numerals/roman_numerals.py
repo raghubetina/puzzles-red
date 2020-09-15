0ba272c60b61a7446bffabd31a50120b5e5364dc
katiezhangbooth <katie.zhang@chicagobooth.edu>
2020-09-15 15:55:58 +0000

    roman numeral converter

--- roman-numerals/roman_numerals.py
+++ roman-numerals/roman_numerals.py
@@ -1,2 +1,16 @@
+number_map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+
+
 def roman(number):
-    pass
+
+    roman = ''
+
+    while number > 0:
+        for i, r in number_map:
+            while number >= i:
+                roman += r
+                number -= i
+
+    return roman;
+
