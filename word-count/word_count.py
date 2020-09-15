4ec8b76542b02eb6ed96386e44abb0477dec4922
katiezhangbooth <katie.zhang@chicagobooth.edu>
2020-09-15 15:18:38 +0000

    first pass at word counter

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,13 @@
 def count_words(sentence):
-    pass
+    counts = dict()
+    words = sentence.split()
+
+    for i in words:
+        if i in counts:
+            counts[i] += 1
+        else:
+            counts[i] = 1
+
+    return counts;
+
+print(count_words('\"That\'s the password: \'PASSWORD 123\'!\", cried the Special Agent.\nSo I fled.'))
