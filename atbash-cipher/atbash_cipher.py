1773ef5a035942f7f418deac1e5f896bb4a10ba1
Jeremy Mopsick <jmopsick@chicagobooth.edu>
2020-09-16 12:00:33 -0500

    cipher-completed

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,17 @@
 def encode(plain_text):
+	backwards = ""
+	i = 0
+	while i <= len(plain_text)-1:
+		backwards = backwards + plain_text[len(plain_text)-1-i]
+		i = i + 1
+	print(backwards)
     
 
 
 def decode(ciphered_text):
-    pass
+	backwards = ""
+	i = 0
+	while i <= len(ciphered_text)-1:
+		backwards = backwards + ciphered_text[len(ciphered_text)-1-i]
+		i = i + 1
+	print(backwards)
